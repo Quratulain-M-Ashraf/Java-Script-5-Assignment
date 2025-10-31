d9187d39cbea1e0618c3e81b0a3a98f8e0afc406
Quratulain Ashraf <anyashraf874@gmail.com>
2025-10-31 12:12:10 -0700

    Add files via upload

---
+++ js 5assignment/app.js
@@ -0,0 +1,45 @@
+
+// JavaScript Function Assignment
+// 1
+function addtwoNumbers(a, b){
+console.log(a + b);
+}
+ addtwoNumbers(8, 5);
+// 2
+function subtracttwoNumbers(a, b){
+console.log(a - b);
+}
+ subtracttwoNumbers(8, 5);
+//  3
+function multiplytwoNumbers(a, b){
+console.log(a * b);
+}
+ multiplytwoNumbers(8, 5);
+//  4
+function dividetwoNumbers(a, b){
+console.log(a / b);
+}
+ dividetwoNumbers(8, 5);
+//  5
+function getRemainder(a, b){
+console.log(a / b);
+}
+ getRemainder(8, 5);
+//  6
+function checknumberinaddition(n){
+    if(n>0){
+        return "Positive";
+    }else{
+        return "Not Positive";
+}
+}
+console.log(checknumberinaddition(5));  
+// 7
+function checknumberinsubtract(n){
+    if(n=-n){
+        return "Negative";
+    }else{
+        return "Not Negative";
+}
+}
+console.log(checknumberinsubtract(5)); 
